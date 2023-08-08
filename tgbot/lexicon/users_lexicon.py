81d3502d062de2c10d5db606c8ce2ae65f594bcd
Masvy <masvy.435@gmail.com>
2023-08-08 20:05:54 +0300

    chore: write text for the bot
    create lexicon(__init__.py, users_lexicon.py) folder
    
    in users_lexicon.py write lines of text

---
+++ tgbot/lexicon/users_lexicon.py
@@ -0,0 +1,12 @@
+MENU: dict[str: str] = {
+    'commands_list': 'Добро пожаловать! 👋\n\nОзнакомьтесь c '
+    'функциями нашего бота:\n🔎 О лиге - /leaguedescription\n'
+    '🤝 Наши партнеры - /ourpartners\n📲 Наш сайт и соцсети - '
+    '/website\n📢 Предложение для партнеров - /offerforpartners\n'
+    '📣 Предложение об участии в Summer CUP SWHL - /offerofparticipation\n'
+    '🏒 Наша арена - /location\n🔥 Можно мне получить стикеры? - /stickers\n'
+    '📋 Подать протест - /protest\n📇 Дозаявить игрока - /new_player\n'
+    '📊 Статистика - /player_statistics\n🏌️ Команды - /commands\n'
+    '📝 Заявить команду - /new_team\n🗓 Расписание - /schedule\n'
+    '⏳ Отсчет до конца сезона - /countdown',
+}
