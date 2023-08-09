5288c0ab04edfcc224f560664884d9e0ab6c422e
Masvy <masvy.435@gmail.com>
2023-08-09 12:42:22 +0300

    feat: create inline-keyboard
    create keyboards(__init__.py, inline.py) folder
    
    in inline.py create keyboard object

---
+++ tgbot/keyboards/inline.py
@@ -0,0 +1,8 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+partners_k: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='SILK WAY RALLY',
+                              url='https://silkwayrally.com/')]
+    ]
+)
