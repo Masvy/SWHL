571a90bb6e37d71b2eb1a4530dec1f7078d0d653
Masvy <masvy.435@gmail.com>
2023-08-09 12:46:50 +0300

    feat: create two new handlers

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -1,12 +1,26 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile
 
-from lexicon.users_lexicon import MENU
+from lexicon.users_lexicon import BASIC_COMMANDS
+from keyboards.inline import partners_k
 
 start_router: Router = Router()
 
 
 @start_router.message(CommandStart())
 async def show_menu(message: Message):
-    await message.answer(text=MENU['commands_list'])
+    await message.answer(text=BASIC_COMMANDS['commands_list'])
+
+
+@start_router.message(Command(commands='leaguedescription'))
+async def show_league_description(message: Message):
+    photo = FSInputFile('tgbot/photo/О лиге.PNG')
+    await message.answer_photo(photo=photo)
+    await message.answer(text=BASIC_COMMANDS['league_description'])
+
+
+@start_router.message(Command(commands='ourpartners'))
+async def show_partners(message: Message):
+    await message.answer(text=BASIC_COMMANDS['our_partners'],
+                         reply_markup=partners_k)
