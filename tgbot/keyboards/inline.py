72e08c550320e3a08894f55ef77df75da46725b4
Masvy <masvy.435@gmail.com>
2023-08-09 18:51:18 +0300

    feat: create new keyboard object

--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -1,23 +1,31 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-partners_k: InlineKeyboardMarkup = InlineKeyboardMarkup(
+partners_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text='SILK WAY RALLY',
                               url='https://silkwayrally.com/')]
     ]
 )
 
-our_links: InlineKeyboardMarkup = InlineKeyboardMarkup(
+our_links_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text='SWHL',
+        [InlineKeyboardButton(text='Сайт SWHL',
                               url='https://swhl.ru/')],
         [
-            InlineKeyboardButton(text='Наш ВК',
+            InlineKeyboardButton(text='ВК',
                                  url='https://vk.com/hockey_rthl'),
-            InlineKeyboardButton(text='Наш YouTube-канал',
+            InlineKeyboardButton(text='YouTube',
                                  url='https://www.youtube.com/c/RTHLTV'),
-            InlineKeyboardButton(text='Наш Telegram-канал',
+            InlineKeyboardButton(text='Telegram',
                                  url='https://t.me/swhleague')
         ]
+    ]
+)
+
+
+stickers_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='SWHL',
+                              url='https://t.me/addstickers/swhlStickers')]
     ]
 )
