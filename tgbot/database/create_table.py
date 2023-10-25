d36cc01d20bda54a11dd1efbd82ea6f430a83a46
Masvy <masvy.435@gmail.com>
2023-10-25 11:58:33 +0300

    refactor: change first table and create sec. table

--- tgbot/database/create_table.py
+++ tgbot/database/create_table.py
@@ -1,18 +1,38 @@
+from sqlalchemy import Column, VARCHAR, BigInteger, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, VARCHAR, DATE, Integer
 
 BaseModel = declarative_base()
 
 
-class NewUser(BaseModel):
-    __tablename__ = 'new_users'
+class NewPlayer(BaseModel):
+    __tablename__ = 'new_players'
 
-    id_ = Column(Integer, unique=True, autoincrement=True, primary_key=True)
+    id_ = Column(Integer, autoincrement=True, primary_key=True)
+
+    user_id = Column(BigInteger, unique=False, nullable=True)
 
     full_name = Column(VARCHAR(50), unique=False, nullable=True)
 
-    birthday = Column(DATE, nullable=True)
+    birthday = Column(VARCHAR(11), unique=False, nullable=True)
 
     stick_grip = Column(VARCHAR(6), unique=False, nullable=True)
 
-    phoro = Column(VARCHAR(100), unique=False, nullable=True)
+    photo = Column(VARCHAR(100), unique=False, nullable=True)
+
+
+class NewCommand(BaseModel):
+    __tablename__ = 'new_commands'
+
+    id_ = Column(Integer, autoincrement=True, primary_key=True)
+
+    user_id = Column(BigInteger, unique=False, nullable=True)
+
+    name = Column(VARCHAR(20), unique=False, nullable=True)
+
+    foundation = Column(VARCHAR(11), unique=False, nullable=True)
+
+    arena = Column(VARCHAR(20), unique=False, nullable=True)
+
+    contact = Column(VARCHAR(50), unique=False, nullable=True)
+
+    phone_number = Column(VARCHAR(11), unique=False, nullable=True)
