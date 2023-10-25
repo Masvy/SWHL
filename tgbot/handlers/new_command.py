0f50ee7675238efe41ba59d06756ee2761a27096
Masvy <masvy.435@gmail.com>
2023-10-25 13:16:18 +0300

    feat: create handlers to check survey data

--- tgbot/handlers/new_command.py
+++ tgbot/handlers/new_command.py
@@ -1,20 +1,29 @@
-from aiogram import Router, F
-from aiogram.types import Message
+from aiogram import Router, types, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
-from aiogram.filters import Command, StateFilter, and_f
+from aiogram.filters import Command, StateFilter
+from aiogram.types import Message, CallbackQuery
 
+from sqlalchemy.orm import sessionmaker
+from keyboards.inline import commands_kb
 from states.users_states import NewCommand
 from lexicon.users_lexicon import NEW_COMMAND
+from database.commands import add_user_id_com, add_name_com, add_foundation, \
+    add_arena, add_contact, add_phone_number
 
 command_router: Router = Router()
 
 
+@command_router.callback_query(F.data == 'wrong_1')
 @command_router.message(Command(commands='new_team'),
                         StateFilter(default_state))
-async def start_new_command(message: Message, state: FSMContext):
-    await message.answer(text=NEW_COMMAND['name'])
-    await state.set_state(NewCommand.name)
+async def start_new_command(update: types.Update, state: FSMContext):
+    if isinstance(update, types.CallbackQuery):
+        await update.message.answer(text=NEW_COMMAND['name'])
+        await state.set_state(NewCommand.name)
+    else:
+        await update.answer(text=NEW_COMMAND['name'])
+        await state.set_state(NewCommand.name)
 
 
 @command_router.message(StateFilter(NewCommand.name))
@@ -47,6 +56,27 @@ async def save_contact(message: Message, state: FSMContext):
 
 @command_router.message(StateFilter(NewCommand.number))
 async def save_number(message: Message, state: FSMContext):
-    await message.answer(text=NEW_COMMAND['data_saved'])
     await state.update_data(number=message.text)
+    data = await state.get_data()
+    await message.answer(text=f'Название команды: {data["name"]}\nГод '
+                         f'основания: {data["foundation"]}\nДомашняя '
+                         f'арена: {data["arena"]}\nФИО контактного лица: '
+                         f'{data["contact"]}\nНомер контактного лица: '
+                         f'{data["number"]}\n\nПроверьте, все ли данные '
+                         'верны?', reply_markup=commands_kb)
+
+
+@command_router.callback_query(F.data == 'right_1')
+async def save_data_1(callback: CallbackQuery,
+                      state: FSMContext,
+                      session_maker: sessionmaker):
+    data = await state.get_data()
+    await add_user_id_com(session_maker, callback.from_user.id)
+    await add_name_com(session_maker, callback.from_user.id, data['name'])
+    await add_foundation(session_maker, callback.from_user.id, data['foundation'])
+    await add_arena(session_maker, callback.from_user.id,
+                    data['arena'])
+    await add_contact(session_maker, callback.from_user.id, data['contact'])
+    await add_phone_number(session_maker, callback.from_user.id, data['number'])
+    await callback.message.edit_text(text=NEW_COMMAND['data_saved'])
     await state.clear()
